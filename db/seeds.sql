418fb0b3bb75dbdbdea95865ce609369ac6d92b5
Amy Gardiner <amymgardiner@gmail.com>
2022-08-01 16:47:19 -0500

    added values for department and roles

---
+++ db/seeds.sql
@@ -0,0 +1,39 @@
+INSERT INTO department (name)
+VALUES
+('Marketing'),
+('Finance'),
+('Human Resources'),
+('Customer Service'),
+('Information Technology');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Marketing Manager', 334400, 1),
+('Copywriter', 68554, 1),
+('Social Media Specialist', 53785, 1),
+('Webmaster', 81500, 1),
+('Graphic Designer', 64903, 1),
+('Video and Audio Creator', 59108, 1),
+('SEO Specialist', 71283, 1),
+('Advertising Specialist', 67800, 1),
+('Public Relations Specialist', 62810, 1),
+('Chief Financial Officer', 415427, 2),
+('Accounts Payable', 55315, 2),
+('Accounts Receivable', 55619, 2),
+('Treasurer', 221516, 2),
+('Tax Accountant', 61093, 2),
+('Payroll Specialist', 51699, 2),
+('Human Resources Manager', 110986, 3),
+('Recruiter', 52179, 3),
+('Employee Relations', 61887, 3),
+('Training', 55507, 3),
+('Health and Safety', 71772, 3),
+('Customer Service Supervisor', 70945, 4),
+('Team Leader', 55946, 4),
+('Agent', 36514, 4),
+('Chief Technology Officer', 257971, 5),
+('Support Desk', 47352, 5),
+('Network Administrator', 65418, 5),
+('Software Developer', 70497, 5),
+('Security Analyst', 79229, 5),
+('Engineer', 95089, 5);
---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const cTable = require('console.table');
+
+// view all departments
+// view all roles
+// view all employees
+
+// formatted table showing department names and department ids
+// the job title, role id, the department that role belongs to, and the salary for that role
+// formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+
+// add a department
+// add a role
+// add an employee
+// update an employee role
+
+// enter the name of the department and that department is added to the database
+// enter the name, salary, and department for the role and that role is added to the database
+//  enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// select an employee to update and their new role and this information is updated in the database
