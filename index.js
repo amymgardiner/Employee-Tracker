e74a05747b5fad1532f5eee4b759425f7ac3b4d5
Amy Gardiner <amymgardiner@gmail.com>
2022-08-01 17:26:55 -0500

    started first inquirer prompt with options from the acceptance criteria

--- index.js
+++ index.js
@@ -2,19 +2,38 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
 
-// view all departments
-// view all roles
-// view all employees
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+);
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+    startApp();
+});
+
+startApp = () => {
+    inquirer.prompt([
+        {
+            name: 'initialPrompt',
+            type: 'list',
+            message: 'Welcome to the employee management program! What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ])
+};
 
 // formatted table showing department names and department ids
 // the job title, role id, the department that role belongs to, and the salary for that role
 // formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-
-// add a department
-// add a role
-// add an employee
-// update an employee role
 
 // enter the name of the department and that department is added to the database
 // enter the name, salary, and department for the role and that role is added to the database
